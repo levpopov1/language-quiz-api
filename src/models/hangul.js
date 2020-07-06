37c7ad7a5062599b34c2adba2a9f24d39ef8bd24
Lev Popov <levpopov1@gmail.com>
2020-07-06 19:51:03 +0100

    adds model for hangul

---
+++ src/models/hangul.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+var HangulSchema = new Schema({
+    kr: {
+        type: String,
+        required: true
+    },
+    en: {
+        type: String,
+        required: true
+    },
+    words:{
+        kr: [String],
+        en: [String]
+    },
+    type:{
+        type: String,
+        enum: ['consonant', 'vowel']
+    }
+});
+
+
+module.exports = mongoose.model('Hangul', HangulSchema, 'hangul');
