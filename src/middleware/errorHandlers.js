3e8bfefb35a5497844c8c02ce4ce7aff0000e794
Lev Popov <levpopov1@gmail.com>
2020-07-08 23:06:09 +0100

    adds 404 error handler

--- src/middleware/errorHandlers.js
+++ src/middleware/errorHandlers.js
@@ -1,12 +1,21 @@
+function notFound(req, res, next){
+    res.status(404).json({
+        statusCode: 404,
+        message: "Not found"
+    });
+}
+
 function genericErrorHandler(err, req, res, next) {
     const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
     res.status(statusCode);
     res.json({
-        message: err.message,
+        statusCode: statusCode,
+        message: `${err}`,
         stack: process.env.NODE_ENV === 'production' ? null : err.stack
     });
 }
 
 module.exports = {
-    genericErrorHandler
+    notFound,
+    genericErrorHandler,
 };
