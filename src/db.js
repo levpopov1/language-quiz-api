01162b99b9d1c78e6b9bd3ef8dc92886da10cd21
Lev Popov <levpopov1@gmail.com>
2020-07-18 20:14:39 +0100

    adds environment specific connection strings

--- src/db.js
+++ src/db.js
@@ -2,14 +2,20 @@
 const mongoose = require('mongoose');
 
 const connectDB = async function(){
+    let connectionString = '';
+    if(process.env.NODE_ENV === "development"){
+        connectionString = process.env.MONGO_DEV_URI;
+    }
+    else{
+        connectionString = `mongodb://${process.env.MONGO_ROOT_USER}:${process.env.MONGO_ROOT_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB_NAME}?authSource=admin`
+    }
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
+        const conn = await mongoose.connect(connectionString, {
             useNewUrlParser: true, 
             useFindAndModify : false, 
             useUnifiedTopology: true
         });
         console.log("MongoDB connetcion successful");
-
     } catch (error) {
         console.error(error);
     }
